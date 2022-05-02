0cb13e329f0f0ffa8249a647e8937563a22f40b6
Mira Weller <mweller@seemoo.tu-darmstadt.de>
2022-05-02 21:32:28 +0200

    fix RESOURCES configuration for sip build

---
+++ project.py
@@ -0,0 +1,31 @@
+
+import os
+
+from pyqtbuild import PyQtBindings, PyQtProject
+from sipbuild import Option
+
+log=open("/tmp/project.log","w")
+print("project py running")
+
+class PyQtAds(PyQtProject):
+	def __init__(self):
+		""" Initialise the project. """
+
+		super().__init__()
+		print("build_dir", self.build_dir)
+
+		self.bindings_factories = [ads]
+
+class ads(PyQtBindings):
+	def __init__(self, project):
+		""" Initialise the bindings. """
+
+		super().__init__(project, 'ads') #, qmake_CONFIG=qmake_CONFIG)
+
+	def apply_user_defaults(self, tool):
+		""" Set default values for user options that haven't been set yet. """
+
+		resource_file = os.path.join(self.project.root_dir,'src','ads.qrc')
+		print("Adding resource file to qmake project: ", resource_file)
+		self.builder_settings.append('RESOURCES += '+resource_file)
+		super().apply_user_defaults(tool)
