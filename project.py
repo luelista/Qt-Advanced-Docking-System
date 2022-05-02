a68858572ed9eb8524dcdfa0957528f1f3be0c1b
Mira Weller <mweller@seemoo.tu-darmstadt.de>
2022-05-02 22:05:00 +0200

    remove leftovers

--- project.py
+++ project.py
@@ -1,11 +1,7 @@
-
 import os
 
 from pyqtbuild import PyQtBindings, PyQtProject
 from sipbuild import Option
-
-log=open("/tmp/project.log","w")
-print("project py running")
 
 class PyQtAds(PyQtProject):
 	def __init__(self):
@@ -19,7 +15,7 @@ class ads(PyQtBindings):
 	def __init__(self, project):
 		""" Initialise the bindings. """
 
-		super().__init__(project, 'ads') #, qmake_CONFIG=qmake_CONFIG)
+		super().__init__(project, 'ads')
 
 	def apply_user_defaults(self, tool):
 		""" Set default values for user options that haven't been set yet. """
@@ -27,4 +23,5 @@ class ads(PyQtBindings):
 		resource_file = os.path.join(self.project.root_dir,'src','ads.qrc')
 		print("Adding resource file to qmake project: ", resource_file)
 		self.builder_settings.append('RESOURCES += '+resource_file)
+		
 		super().apply_user_defaults(tool)
